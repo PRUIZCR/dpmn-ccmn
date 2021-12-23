14c5e9ea8c71f26a6cdf79275720d23adaa07fe3
pruizcr <friendpaul2020@gmail.com>
2021-12-23 14:10:13 -0500

    Add files via upload

---
+++ bean/param-busquedaccmn.ts
@@ -0,0 +1,29 @@
+
+import { DataCatalogo } from "../common/data-catalogo.model";
+export class ConsultaCcmn {
+    codigoTransportista: string;
+    rucTransportista: string;
+    razonSocialTransportista: string;
+    tipoDocumentoDescarga: DataCatalogo;
+    estadoDelDocumentoDeDescarga: DataCatalogo;
+    tipoDeControl: DataCatalogo;
+    paisPlacaDelVehiculo: DataCatalogo;
+    placaDelVehiculo: string;
+    indicadorPorDocumento: boolean;
+    numeroDeDescarga: number;
+    aduanaDeDescarga: DataCatalogo;
+    puestoDeControlDescarga: DataCatalogo;
+    anoDeDescarga: number;
+    rucDelRemitente: string;
+    razonSocialDelRemitente: string;
+    indicadorPorDeclaracion: boolean;
+    aduanaDeDeclaracion: DataCatalogo;
+    anoDeDeclaracion: number;
+    regimenDeDeclaracion: DataCatalogo;
+    numeroDeDeclaracion: number;
+    indicadorPorFecha: boolean;
+    fechaDeInicioConsulta: string;
+    fechaFinConsulta: string;
+    indicadorConsultaFuncionario: boolean;
+    codAduanaFuncionario:string;
+}
